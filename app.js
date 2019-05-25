0e35c2460cd8db2a8dc6394a6d7f573fd5d77296
ade rahmanp <aderahmanp@gmail.com>
2019-05-25 16:11:19 +0700

    boiler plat

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const userRoutes = require("./routes/user");
+const mongoose = require("mongoose");
+
+try {
+  dotEnv = require("dotenv").config();
+} catch (err) {
+  console.log(err.message);
+}
+
+// Initial PORT
+
+const PORT = process.env.PORT || 1234;
+const mongoUrl = process.env.MONGGO_DB_URL;
+
+// Connecting Mongoose
+mongoose.set("userCreateIndex", true);
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`Mongoose connected at ${mongoUrl}`);
+  })
+  .catch(err => {
+    throw new Error(err);
+  });
+
+// Running Express Application
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// initialzation of plug'in Middleware, Routes
+app.use("/", userRoutes);
+
+// Listen server
+app.listen(PORT, () => {
+  console.log(`Servet is now listening on port ${PORT}`);
+});
+
+module.exports = app;
