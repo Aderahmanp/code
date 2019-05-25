7220dbcf4f440e98923f0f2b67ae99bde57da972
ade rahmanp <aderahmanp@gmail.com>
2019-05-25 16:18:32 +0700

    find all user

--- controllers/user.js
+++ controllers/user.js
@@ -18,3 +18,20 @@ exports.Create = (req, res) => {
       });
     });
 };
+
+exports.find = (req, res) => {
+  User.find({})
+    .then(data => {
+      res.status(200).json({
+        success: true,
+        message: "Successful",
+        data: data
+      });
+    })
+    .catch(err => {
+      res.status(400).json({
+        success: false,
+        message: err.message
+      });
+    });
+};
