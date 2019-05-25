bbf9c4f725cf04d07050b0f3e1d582cee8bcf241
ade rahmanp <aderahmanp@gmail.com>
2019-05-25 10:51:46 +0700

    Nomer 1

---
+++ 1.js
@@ -0,0 +1,26 @@
+function biodata() {
+  let Ade = {
+    Name: "Ade Rahman P",
+    Address: "Depok",
+    Hobbies: ["Read Book", "Basketball", "Streaming"],
+    isMarried: false,
+    School: {
+      highSchool: "Sejahtera 1",
+      University: "UPNVJ"
+    },
+    Skill: [
+      {
+        nodeJs: 75
+      },
+      {
+        mongodb: 80
+      },
+      {
+        express: 70
+      }
+    ]
+  };
+  return Ade;
+}
+
+console.log(biodata());
